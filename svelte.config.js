ef197aa16f358cd7f358ebef3c3086f5c2a5ef40
Sneha Shah <snehaeshah@gmail.com>
2024-10-18 19:16:03 -0700

    GitHub Actions Second Time

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,15 @@
-import adapter from '@sveltejs/adapter-static';Cannot find module '@sveltejs/adapter-static' or its corresponding type declarations.2307Cannot find module '@sveltejs/adapter-static' or its corresponding type declarations.
-/** @type {import('@sveltejs/kit').Config} */const config = {	kit: {		adapter: adapter({			fallback: '404.html'		}),		paths: {			base: process.argv.includes('dev') ? '' : process.env.BASE_PATHType 'string | undefined' is not assignable to type '"" | `/${string}` | undefined'.
-  Type 'string' is not assignable to type '"" | `/${string}` | undefined'.2322Type 'string | undefined' is not assignable to type '"" | `/${string}` | undefined'.
-  Type 'string' is not assignable to type '"" | `/${string}` | undefined'.		}	}};
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}
+};
+
 export default config;
