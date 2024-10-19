0cd767cea5a5095598bdd546c39040b38548c3d1
Sneha Shah <snehaeshah@gmail.com>
2024-10-18 19:08:17 -0700

    GitHub Actions

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
-};
-
+import adapter from '@sveltejs/adapter-static';Cannot find module '@sveltejs/adapter-static' or its corresponding type declarations.2307Cannot find module '@sveltejs/adapter-static' or its corresponding type declarations.
+/** @type {import('@sveltejs/kit').Config} */const config = {	kit: {		adapter: adapter({			fallback: '404.html'		}),		paths: {			base: process.argv.includes('dev') ? '' : process.env.BASE_PATHType 'string | undefined' is not assignable to type '"" | `/${string}` | undefined'.
+  Type 'string' is not assignable to type '"" | `/${string}` | undefined'.2322Type 'string | undefined' is not assignable to type '"" | `/${string}` | undefined'.
+  Type 'string' is not assignable to type '"" | `/${string}` | undefined'.		}	}};
 export default config;
